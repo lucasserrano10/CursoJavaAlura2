85e338157c5bc43103cd2c43cc9863ee6a90706a
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2025-07-31 21:15:07 -0300

    Iniciando a aplicação de Spring Security

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("medicos")
-public class MedicoController {
+public class   MedicoController {
 
     @Autowired
     private MedicoRepository repository;
